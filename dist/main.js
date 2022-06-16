128cb03464c04e9746b1c351a70a7bdeb4b704fc
garretthilberling <ghilberling@knights.ucf.edu>
2022-06-15 20:41:09 -0400

    learned about what data types to avoid and their alternatives

--- dist/main.js
+++ dist/main.js
@@ -9,3 +9,22 @@ var username = 'alex';
 var pageName = '1';
 var errorMessage = null;
 var user = null;
+////////////////////////////////////////////////////////////////////////////////////////////
+var doSomething = function () {
+    console.log('doSomething');
+};
+// never expects function to return something that will never happen/never ends
+var never = function () {
+    throw 'never';
+};
+// avoid using any no matter what. it is not a solution it is just a start of bigger problems in your project
+var foo = 'foo';
+var vAny = 10;
+var vUnknown = 10; // type unknown is a good alternative to type any
+var s1 = vAny;
+// to avoid error
+var s2 = vUnknown; // will not work because we are telling TypeScript we do not know the type
+var pageNumber = '1';
+var numericPageNumber = pageNumber; // known as type assertion
+// console.log(vAny.foo());
+// console.log(vUnknown.foo()); // will give error: property 'foo' does not exist
--- src/main.ts
+++ src/main.ts
@@ -54,3 +54,30 @@ let pageName: string | number = '1';
 let errorMessage: string | null = null;
 
 let user: UserInterface | null = null;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+const doSomething = (): void => {
+    console.log('doSomething');
+}
+
+// never expects function to return something that will never happen/never ends
+const never = (): never => {
+    throw 'never';
+}
+
+// avoid using any no matter what. it is not a solution it is just a start of bigger problems in your project
+let foo: any = 'foo';
+
+let vAny: any = 10;
+let vUnknown: unknown = 10; // type unknown is a good alternative to type any
+
+let s1: string = vAny;
+                    // to avoid error
+let s2: string = vUnknown as string; // will not work because we are telling TypeScript we do not know the type
+
+let pageNumber: string = '1';
+let numericPageNumber: number = pageNumber as unknown as number; // known as type assertion
+// console.log(vAny.foo());
+// console.log(vUnknown.foo()); // will give error: property 'foo' does not exist
