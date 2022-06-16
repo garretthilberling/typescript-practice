ad4cd99ad3cbc42ba51ab3c75dd2e03bf12091d0
garretthilberling <ghilberling@knights.ucf.edu>
2022-06-15 20:23:55 -0400

    learned about type aliases and unions

--- dist/main.js
+++ dist/main.js
@@ -3,17 +3,9 @@ var getFullName = function (name, surname) {
     return name + ' ' + surname;
 };
 console.log(getFullName('Garrett', 'Hilberling'));
-var user = {
-    name: 'Garrett',
-    age: 23,
-    getMessage: function () {
-        return "Hello " + name;
-    }
-};
-var user2 = {
-    name: 'Jack',
-    getMessage: function () {
-        return "Hello " + name;
-    }
-};
-console.log(user.name);
+var popularTags = ['dragon', 'coffee'];
+var dragonsTag = 'dragon';
+var username = 'alex';
+var pageName = '1';
+var errorMessage = null;
+var user = null;
--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,50 @@ console.log(getFullName('Garrett', 'Hilberling'));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
+// interface UserInterface {
+//     name: string;
+//     // ? means not mandatory
+//     age?: number;
+//     getMessage(): string;
+// }
+
+// const user: UserInterface = {
+//     name: 'Garrett',
+//     age: 23,
+//     getMessage() {
+//         return "Hello " + name;
+//     }
+// };
+
+// const user2: UserInterface = {
+//     name: 'Jack',
+//     getMessage() {
+//         return "Hello " + name;
+//     }
+// }
+
+// console.log(user.name);
+
+
+/////////////////////////////////////////////////////////////////////////////
+type ID = string;
+type PopularTag = string;
+type MaybePopularTag = PopularTag | null;
+
 interface UserInterface {
+    id: ID;
     name: string;
-    // ? means not mandatory
-    age?: number;
-    getMessage(): string;
+    surname: string;
 }
 
-const user: UserInterface = {
-    name: 'Garrett',
-    age: 23,
-    getMessage() {
-        return "Hello " + name;
-    }
-};
+const popularTags: PopularTag[] = ['dragon', 'coffee'];
 
-const user2: UserInterface = {
-    name: 'Jack',
-    getMessage() {
-        return "Hello " + name;
-    }
-}
+const dragonsTag: MaybePopularTag = 'dragon';
 
-console.log(user.name);
+let username: string = 'alex';
+
+let pageName: string | number = '1';
+
+let errorMessage: string | null = null;
+
+let user: UserInterface | null = null;
