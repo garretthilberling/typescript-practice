9689c6f17c55f26c5c5dcf29d39355240e8238ad
garretthilberling <ghilberling@knights.ucf.edu>
2022-06-15 20:01:14 -0400

    initial set up. Learning about how types can be used to safely structure variables and functions

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+// function must return string
+var getFullName = function (name, surname) {
+    return name + ' ' + surname;
+};
+console.log(getFullName('monster', 'lessons'));
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+                                                    // function must return string
+const getFullName = (name: string, surname: string): string => {
+    return name + ' ' + surname;
+}
+
+console.log(getFullName('monster', 'lessons'));
