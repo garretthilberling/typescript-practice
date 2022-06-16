323caf87a28bf2ae99a964e014df5576a8e5a04b
garretthilberling <ghilberling@knights.ucf.edu>
2022-06-15 20:12:46 -0400

    learned about interfaces and how they can be instrumental in defining the architecture of a given project

--- dist/main.js
+++ dist/main.js
@@ -2,4 +2,18 @@
 var getFullName = function (name, surname) {
     return name + ' ' + surname;
 };
-console.log(getFullName('monster', 'lessons'));
+console.log(getFullName('Garrett', 'Hilberling'));
+var user = {
+    name: 'Garrett',
+    age: 23,
+    getMessage: function () {
+        return "Hello " + name;
+    }
+};
+var user2 = {
+    name: 'Jack',
+    getMessage: function () {
+        return "Hello " + name;
+    }
+};
+console.log(user.name);
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,30 @@ const getFullName = (name: string, surname: string): string => {
     return name + ' ' + surname;
 }
 
-console.log(getFullName('monster', 'lessons'));
+console.log(getFullName('Garrett', 'Hilberling'));
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+interface UserInterface {
+    name: string;
+    // ? means not mandatory
+    age?: number;
+    getMessage(): string;
+}
+
+const user: UserInterface = {
+    name: 'Garrett',
+    age: 23,
+    getMessage() {
+        return "Hello " + name;
+    }
+};
+
+const user2: UserInterface = {
+    name: 'Jack',
+    getMessage() {
+        return "Hello " + name;
+    }
+}
+
+console.log(user.name);
